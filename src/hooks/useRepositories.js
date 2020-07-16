4ab66712d62834ba35f15bb675b202f776300ac9
hnrqss <hnrq.assuncao@gmail.com>
2020-07-15 22:00:03 -0300

    Corrigindo lógica deletar item

--- src/hooks/useRepositories.js
+++ src/hooks/useRepositories.js
@@ -6,13 +6,16 @@ export default function useRepositories() {
     const [ repositories, setRepositories ] = React.useState([])
 
     React.useEffect(() => {
+
+        async function fetchRepositories() {
+            const { data } = await api.get('/repositories')
+            setRepositories(data || [])
+        }
+
         fetchRepositories()
     }, [])
 
-    async function fetchRepositories() {
-        const { data } = api.get('/repositories')
-        setRepositories(data || [])
-    }
+    
 
     async function handleAddRepository(repo) {
         const { data } = await api.post('/repositories', repo)        
@@ -20,9 +23,15 @@ export default function useRepositories() {
     }
     
     async function handleRemoveRepository(id) {
+        
+        console.log('repo', id)
+
+        const repos = repositories.find(repository => repository.id !== id) || []
+
+        setRepositories(repos)
+        
         await api.delete(`/repositories/${id}`)
-        fetchRepositories()
-    }
+    }   
 
     
     return {
